d604f4e8b7d7cff79c706dcc37f09d07d36a3978
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-05-30 18:19:28 +0300

    Built AddTodo functionality

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,15 +1,46 @@
-import { FaPlus, FaCalendarAlt } from 'react-icons/fa';
+import { useState } from 'react';
+
+interface Todo {
+	id: number;
+	text: string;
+}
 
 const AddTodo = () => {
+	const [todos, setTodos] = useState<Todo[]>([]);
+	const [value, setValue] = useState('');
+
+	const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value);
+	};
+
+	const handleAddTodo = () => {
+		if (!value) return;
+		const newTodo: Todo = {
+			id: todos.length + 1,
+			text: value,
+		};
+
+		console.log(newTodo);
+
+		setTodos([...todos, newTodo]);
+		setValue('');
+	};
+
 	return (
 		<div className="container mx-auto flex justify-between gap-2 p-4">
 			<input
 				type="text"
 				className="grow rounded-lg border border-gray-400 px-4 py-2 focus:outline-none"
 				placeholder="Add Todo..."
+				value={value}
+				onChange={handleInput}
 			/>
-			<button className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-400">
-				Add
+			<button
+				className="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-400"
+				type="submit"
+				onClick={handleAddTodo}
+			>
+				Add Todo
 			</button>
 		</div>
 	);
