e49ebcf89dddac7305664a8ceb99fefdbe0d15c1
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-06-01 17:57:41 +0300

    Implemented local storage for todos useState

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,20 @@ import { Todo } from './components/AddTodo';
 import CompletedTodos from './components/ComletedTodos';
 
 function App() {
-	// // store todos in local storage to persist data on refresh or empty list
-	// const [todos, setTodos] = useState<Todo[]>(() => {
-	// 	const localData = localStorage.getItem('todos');
-	// 	return localData ? JSON.parse(localData) : [];
-	// });
+	// store todos in local storage to persist data on refresh or empty list
+	const [todos, setTodos] = useState<Todo[]>(() => {
+		const localData = localStorage.getItem('todos');
+		return localData ? JSON.parse(localData) : [];
+	});
 
-	// // update local storage when todos change
-	// localStorage.setItem('todos', JSON.stringify(todos));
+	// update local storage when todos change
+	localStorage.setItem('todos', JSON.stringify(todos));
 
-	const [todos, setTodos] = useState<Todo[]>([
-		{ id: 1, text: 'Learn React', completed: false },
-		{ id: 2, text: 'Learn TypeScript', completed: false },
-		{ id: 3, text: 'Learn Tailwind CSS', completed: false },
-	]);
+	// const [todos, setTodos] = useState<Todo[]>([
+	// 	{ id: 1, text: 'Learn React', completed: false },
+	// 	{ id: 2, text: 'Learn TypeScript', completed: false },
+	// 	{ id: 3, text: 'Learn Tailwind CSS', completed: false },
+	// ]);
 
 	return (
 		<>
